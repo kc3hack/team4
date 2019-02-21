d83e40e8650fff497b0aed18b28416c7b127e6b8
flaireclair <h.kmntag6.gaburiasu@gmail.com>
2019-02-21 14:02:16 +0900

    gyuho is freegit add -Agit add -A!

--- api/googlemaps.go
+++ api/googlemaps.go
@@ -6,7 +6,7 @@ import (
 	"googlemaps.github.io/maps"
 )
 
-func Maps(ori string, dest string) []maps.Route{
+func MapsWithWayPoint(ori string, dest string, mode maps.Mode, way []string) []maps.Route{
 	c, err := maps.NewClient(maps.WithAPIKey("AIzaSyDvq_9Eg7Reuu83LQUIzu-e-tKZE7GkIj4"))
 	if err != nil {
 		log.Fatalf("fatal error1: %s", err)
@@ -14,6 +14,26 @@ func Maps(ori string, dest string) []maps.Route{
 	r := &maps.DirectionsRequest{
 		Origin: 		ori,
 		Destination:	dest,
+		Mode:			mode,
+		Waypoints:		way,
+	}
+	route, _, err := c.Directions(context.Background(), r)
+	if err != nil {
+		log.Fatalf("fatal error2: %s", err)
+	}
+
+	return route
+}
+
+func Maps(ori string, dest string, mode maps.Mode) []maps.Route{
+	c, err := maps.NewClient(maps.WithAPIKey("AIzaSyDvq_9Eg7Reuu83LQUIzu-e-tKZE7GkIj4"))
+	if err != nil {
+		log.Fatalf("fatal error1: %s", err)
+	}
+	r := &maps.DirectionsRequest{
+		Origin: 		ori,
+		Destination:	dest,
+		Mode:			mode,
 	}
 	route, _, err := c.Directions(context.Background(), r)
 	if err != nil {
--- server.go
+++ server.go
@@ -8,23 +8,47 @@ import (
     "io/ioutil"
     "bytes"
     "net/http"
+    "googlemaps.github.io/maps"
 )
 
+type CreateParams struct{
+    OriginPoint OriginPoint `json:"originpoint"`
+    DestinationPoint DestinationPoint `json:"destinationpoint"`
+    WayPoint WayPoint `json:"waypoint"`
+    Ways maps.Mode `json:"ways"`
+}
 
 type Data struct {
-    Anime_id int64 `json:"anime_id"`
-	Area_id int64 `json:"area_id"`
 	Sanc_id int64 `json:"sanc_id"`
+    Anime_id int64 `json:"anime_id"`
+	//Area_id int64 `json:"area_id"`
 	Place_id string `json:"place_id"`
 }
+
+type OriginPoint struct{
+    OriginData Data `json:"origindata"`
+}
+
+type DestinationPoint struct{
+    DestinationData Data `json:"destinatindata"`
+}
+
+type WayPoint struct{
+    WayData WayData `json:"waydataarr"`
+}
+
+type WayData []*Data
 
 
 type Data_arr []*Data
 
 func main() {
     r := gin.Default()
-    var data_arr Data_arr
-    place_arr := []string{}
+    var way_data_arr WayData
+    data_arr := []string{}
+    //place_arr := []string{}
+    //var waypoint_data_arr = WayData
+    var params CreateParams
     r.GET("/", func(c *gin.Context) {
         c.String(200, "今日は世界!")
     })
@@ -38,8 +62,13 @@ func main() {
         c.JSON(200, api.Areas_list())
     })
     r.GET("/direction", func(c *gin.Context){
-        c.IndentedJSON(200, api.Maps(place_arr[0], place_arr[len(place_arr)-1]))
-        pretty.Println(api.Maps("Rokujizo Station", "Port Island Kita Park"))
+        if params.WayPoint.WayData != nil {
+            c.IndentedJSON(200, api.MapsWithWayPoint(params.OriginPoint.OriginData.Place_id, params.DestinationPoint.DestinationData.Place_id, params.Ways, data_arr))
+            pretty.Println(api.MapsWithWayPoint(params.OriginPoint.OriginData.Place_id, params.DestinationPoint.DestinationData.Place_id, params.Ways, data_arr))
+        }else{
+            c.IndentedJSON(200, api.Maps(params.OriginPoint.OriginData.Place_id, params.DestinationPoint.DestinationData.Place_id, params.Ways))
+            pretty.Println(api.Maps(params.OriginPoint.OriginData.Place_id, params.DestinationPoint.DestinationData.Place_id, params.Ways))
+        }
     })
     r.POST("/json", func(c *gin.Context){
 
@@ -49,8 +78,6 @@ func main() {
         c.Request.Body = ioutil.NopCloser(bytes.NewBuffer([]byte(b)))
         fmt.Println(b)
 
-        var params Data
-
         if err := c.BindJSON(&params); err != nil {
             //c.Warningf("Failed binding request parameters: %s", err.Error())
             c.Status(http.StatusBadRequest)
@@ -58,12 +85,22 @@ func main() {
         }
 
         c.JSON(200, params)
-        data_arr = append(data_arr, &params)
-        for _, i := range data_arr {
-            fmt.Println(i.Place_id)
+        //data_arr = append(data_arr, &params)
+        //for _, i, j, k, l := range params {
+            //fmt.Println(i.Place_id)
+        //}
+
+        fmt.Println(params)
+
+        if params.WayPoint.WayData != nil {
+            way_data_arr = params.WayPoint.WayData
+            for _, i := range way_data_arr {
+                fmt.Println(i.Place_id)
+                data_arr = append(data_arr, i.Place_id)
+            }
         }
 
-        place_arr = append(place_arr, data_arr[len(data_arr)-1].Place_id)
+        //place_arr = append(place_arr, data_arr[len(data_arr)-1].Place_id)
         //c.JSON(200, gin.H{"1": foo.Anime_id, "2": foo.Area_id})
     })
     r.Run()

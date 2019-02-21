d5fee1e96a1ce660d5ea30803dbfb59788a1d8e1
niya <yoheikurioka@gmail.com>
2019-02-21 09:58:11 +0900

    server.goでdatabeseのやつ動いてた奴がundoで消えてしまったので,あとで書きます

--- api/animes.go
+++ api/animes.go
@@ -3,31 +3,40 @@ package api
 type Animes struct {
 	Anime_ID int `json:"anime_id"`
 	Anime_Name string `json:"anime_name"`
+	Image string `json:"image"`
 	Anime_details Anime_details `json:"animes_details"`
 }
 
 type Anime_details struct{
-	Area_Name string `json:"area_name"`
 	Area_ID int `json:"area_id"`
 	Url string `json:"url"`
 }
 
-type Animes_Lists struct{
-	Anime_List []Animes `json:"anime_list"`
+type ResponseData struct {
+	Datas        []Animes_Lists   `json:"datas"`
 }
 
-func GetAnimes() Animes_Lists{
-	return Animes_Lists{
-		Anime_List: []Animes{
-			Animes{
-				Anime_ID: 1,
-				Anime_Name: "けいおん！",
-				Anime_details: Anime_details{
-					Area_Name: "滋賀県犬上群豊郷町",
-					Area_ID: 1,
-					Url: "http://www.tbs.co.jp/anime/k-on/",
-				},
-			},
-		},
-	}
-}	
+type Animes_Lists struct{
+	// Anime_List []Animes `json:"anime_list"`
+	Anime_ID int `json:"anime_id"`
+	Anime_Name string `json:"anime_name"`
+	Area_ID int `json:"area_id"`
+	Image string `json:"image"`
+	Url string `json:"url"`
+}
+
+// func GetAnimes() Animes_Lists{
+// 	return Animes_Lists{
+// 		Anime_List: []Animes{
+// 			Animes{
+// 				Anime_ID: 1,
+// 				Anime_Name: "けいおん！",
+// 				Image: "",
+// 				Anime_details: Anime_details{
+// 					Area_ID: 1,
+// 					Url: "http://www.tbs.co.jp/anime/k-on/",
+// 				},
+// 			},
+// 		},
+// 	}
+// }	
